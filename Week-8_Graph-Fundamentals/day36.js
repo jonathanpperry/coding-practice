44d9e3228a83114840985648c587a4c61fd2923b
Jon Perry <jonperry113@gmail.com>
2026-08-31 13:59:16 -0400

    Complete day 1 of week 8 (graph fundamentals)

---
+++ Week-8_Graph-Fundamentals/day36.js
@@ -0,0 +1,46 @@
+// Problem 1: Number of Islands
+
+// Given an m x n grid containing "1" for land and "0" for water, return the number of islands. Land connects horizontally and vertically, not diagonally.
+
+// const grid = [
+//   ["1", "1", "0", "0", "0"],
+//   ["1", "1", "0", "0", "0"],
+//   ["0", "0", "1", "0", "0"],
+//   ["0", "0", "0", "1", "1"]
+// ];
+
+// // Expected: 3
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+  let count = 0;
+
+  function dfs(i, j) {
+    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === "0") {
+      return;
+    }
+
+    grid[i][j] = "0";
+
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "1") {
+        dfs(i, j);
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
