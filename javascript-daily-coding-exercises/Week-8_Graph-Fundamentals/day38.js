7a3c8df51a6d71f269179baa23e0428f2deae082
Jon Perry <jonperry113@gmail.com>
2026-09-02 18:31:05 -0400

    Add day 3 of week 8 (graph fundamentals)

--- javascript-daily-coding-exercises/Week-8_Graph-Fundamentals/day38.js
+++ javascript-daily-coding-exercises/Week-8_Graph-Fundamentals/day38.js
@@ -1,6 +1,6 @@
 // Course Schedule.
 
-// You have numCourses courses labeled 
+// You have numCourses courses labeled
 // from 0 to numCourses - 1.
 
 // You’re given prerequisites, where:
@@ -15,6 +15,51 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function(numCourses, prerequisites) {
-    
+var canFinish = function (numCourses, prerequisites) {
+  const graph = new Map();
+  const visited = new Set();
+  const visiting = new Set();
+
+  // Build graph
+  for (const [course, prereq] of prerequisites) {
+    if (!graph.has(prereq)) {
+      graph.set(prereq, []);
+    }
+    graph.get(prereq).push(course);
+  }
+
+  function dfs(course) {
+    if (visiting.has(course)) {
+      // cycle!
+      return false;
+    }
+
+    if (visited.has(course)) {
+      // already safely processed
+      return true;
+    }
+
+    visiting.add(course);
+
+    // DFS through prerequisites/neighbors...
+    const neighbors = graph.get(course) || [];
+
+    for (const neighbor of neighbors) {
+      if (!dfs(neighbor)) {
+        return false;
+      }
+    }
+
+    visiting.delete(course);
+    visited.add(course);
+    return true;
+  }
+
+  for (let course = 0; course < numCourses; course++) {
+    if (!dfs(course)) {
+      return false;
+    }
+  }
+
+  return true;
 };
